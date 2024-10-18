61d2625784b4b8c4c5d9d82a58c09a133850e315
Filipe Alves Vieira <131316142+israelvalente@users.noreply.github.com>
2024-10-18 18:42:13 -0300

    the second

--- finding.py
+++ finding.py
@@ -1,8 +1,10 @@
 while True:    
     phrase = 'The most important thing in the world is coffee.'
     print('- ' * 35)
+
     letter = str(input('Digite uma letra ')).lower()[0]
     print('- ' * 35)
+
     number = phrase.count(letter)
     if number > 1:
         print(f'Após uma profunda análise, encontramos a letra {letter} {number} vezes na string')
@@ -10,6 +12,7 @@ while True:
         print(f'Após uma profunda análise, encontramos a letra {letter} apenas {number} vez na string')
     elif number <= 0:
         print(f'Após uma profunda análise, NÃO encontramos a letra {letter} na string')
+        
     print('- ' * 35)
     answer = input('Deseja continuar? [S/N] ').strip().upper()[0]
     if answer in 'N':
